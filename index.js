e00472f00bbb48d19b209f29fe0f4ddd199a6bc1
Jacob Ashdown <jcbashdown@gmail.com>
2016-08-24 13:45:57 +0100

    Half day hack - Lingala flashcards
    todo:
    - need to bundle dictionary so people have a chance - show success by
      words
    - need to add verbs with conjugations
    - just verbs mode
    - some kind of learning mode - 10 words at a time, flash translations
      first. Then do until you have got all right 3 times in a row. Readd
      words after 10 days.

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  TouchableOpacity,
+  ListView,
   View
 } from 'react-native';
 import Dimensions from 'Dimensions'
@@ -18,12 +20,11 @@ const chooseRandomFour = function(lingalaData) {
   var results = {testSubject: {}, randomFour: []};
   var keys = Object.keys(lingalaData);
   for(i = 0; i < 4; i++) {
-    var key = keys[Math.floor(Math.random(keys.length))];
-    var result = {};
-    result[key] = lingalaData[key];
+    var key = keys[Math.floor((Math.random() * keys.length) + 1) - 1];
+    var result = {index: i, lingala: key, english: lingalaData[key]};
     results['randomFour'].push(result);
   }
-  results['testSubject'] = results.randomFour[0];
+  results['testSubject'] = results.randomFour[Math.floor((Math.random() * 4) + 1) - 1];
   return results;
 }
 
@@ -31,36 +32,116 @@ class lingala extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
+      correctNumber: 0,
+      incorrectNumber: 0,
       currentTest: chooseRandomFour(lingalaData)
     }
   }
   next() {
-    this.setState = {
-      currentTest: chooseRandomFour(lingalaData)
-    };
+    this.setState({
+      currentTest: chooseRandomFour(lingalaData),
+      correct: undefined, 
+      correctIndex: undefined 
+    });
+  }
+  choose(index) {
+    var correct;
+    var correctNumber = this.state.correctNumber;
+    var incorrectNumber = this.state.incorrectNumber;
+    if(index === this.state.currentTest.testSubject.index) {
+      correctNumber++; 
+      correct = true;
+    } else {
+      incorrectNumber++; 
+      correct = false;
+    }
+    this.setState({
+      correct: correct,
+      correctNumber: correctNumber,
+      incorrectNumber: incorrectNumber,
+      correctIndex: this.state.currentTest.testSubject.index 
+    });
   }
   componentWillMount() {
     this.state.currentTest = chooseRandomFour(lingalaData); 
   }
+  getStyle(index) {
+    if(this.state.correct !== undefined) {
+      if(index === this.state.correctIndex) {
+        return styles.correctResponse;
+      } else {
+        return styles.incorrectResponse;
+      }
+    } else {
+      return styles.normalPossibleResponse;
+    }
+
+  }
+  getOptionsList(options) {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var dataSource = ds.cloneWithRows(options) 
+    return (
+      <View style={styles.quarter}>
+        <ListView
+          styles={styles.quarter}
+          horizontal={true}
+          dataSource={dataSource}
+          renderRow={(rowData) => {
+            if(this.state.correct === undefined) {
+              return (<View style={this.getStyle(rowData.index)}>
+                <TouchableOpacity style={styles.touch} onPress={() => {this.choose(rowData.index)}}>
+                  <View>
+                    <Text>
+                      {rowData['lingala']}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>)
+            } else {
+              return (<View style={this.getStyle(rowData.index)}>
+                <View>
+                  <Text>
+                    {rowData['lingala']}
+                  </Text>
+                </View>
+              </View>)
+            }
+          }}
+        />
+      </View>
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.topBar}>
+          <View style={styles.quarterWidth}>
+            <Text style={styles.topText}>
+              {"Correct: " + this.state.correctNumber}
+            </Text>
+          </View>
+          <View style={styles.quarterWidth}>
+            <Text style={styles.topText}>
+              {"Incorrect: " + this.state.incorrectNumber}
+            </Text>
+          </View>
+          <View style={styles.quarterWidth}></View>
+          <View style={styles.quarterWidth}>
+            <TouchableOpacity onPress={this.next.bind(this)}>
+              <Text style={styles.topText}>
+                Next 
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
         <View style={styles.topHalf}>
           <Text>
-            {this.state.currentTest.testSubject}}
-          </Text>
-          <Text style={styles.instructions}>
-            To get started, edit index.android.js
-          </Text>
-          <Text style={styles.instructions}>
-            Double tap R on your keyboard to reload,{'\n'}
-            Shake or press menu button for dev menu
+            {this.state.currentTest.testSubject['english']}
           </Text>
         </View>
         <View style={styles.bottomHalf}>
-          <Text style={styles.welcome}>
-            Welcome to React Native!
-          </Text>
+          {this.getOptionsList(this.state.currentTest.randomFour.slice(0, 2))}
+          {this.getOptionsList(this.state.currentTest.randomFour.slice(2, 4))}
         </View>
       </View>
     );
@@ -73,17 +154,22 @@ var {
 } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-    flexDirection: 'column'
   },
-  half: {
-    height: height / 2,
+  topHalf: {
+    height: (height / 2) - 50,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#F5FCFF',
+  },
+  bottomHalf: {
+    height: (height / 2),
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
     backgroundColor: '#F5FCFF',
   },
   quarter: {
@@ -94,14 +180,56 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     flexDirection: 'row'
   },
-  eighth: {
+  normalPossibleResponse: {
     height: height / 4,
-    width: width / 8,
+    width: width / 2,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignSelf: 'stretch',
+    backgroundColor: 'blue'
   },
+  correctResponse: {
+    height: height / 4,
+    width: width / 2,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: 'green'
+  },
+  incorrectResponse: {
+    height: height / 4,
+    width: width / 2,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: 'red'
+  },
+  topBar: {
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black'
+  },
+  threeQuartersWidth: {
+    width: (width / 4) * 3
+  },
+  quarterWidth: {
+    paddingLeft: 15,
+    width: width / 4 
+  },
+  topText: {
+    color: 'white'
+  },
+  touch: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    flex: 1
+  }
 });
 
 export default lingala 
